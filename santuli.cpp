8fcb3d39e5db0fea1b9bab76cc03b30b4b1dce90
Santipili <santi.tomas.pili@gmail.com>
2021-05-31 11:08:14 -0300

    elimine un archivo de prueba

--- santuli.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-//using namespace std;
-//borre el "using namespace std" porque se lo agregue adelante de las variables (palabras claves de la biblioteca)
-
-int main(){
-    std::cout << "hola mundo" << std::endl;
-    return 0;
-}
-
