6a898f602fdd5588ccd75138cb3b7434b8cf658b
Santipili <santi.tomas.pili@gmail.com>
2021-07-10 13:52:39 -0300

    actualizacion no final

--- TP_4/vectores.cpp
+++ TP_4/vectores.cpp
@@ -22,43 +22,61 @@ typedef struct{
 
 void cargaDeArchivo (const string path, tLista& LISTA);
 int menuPrincipal ();
-void showList(tLista& LISTA);
+void showList(tLista LISTA);
 void addNew (tLista& LISTA);
 void changeOne (tLista& LISTA);
+void saveList (const string path, tLista& LISTA);
+void deleteOne (tLista& LISTA);
+void changeNotes(tLista& LISTA);
+void clasificateNotes (tLista LISTA);
+int calculatePromedio (tLista LISTA);
 
 int main () {
-
     tLista lista;
-
-    // cargar lista
-
-    cargaDeArchivo("AyED1.txt", lista);
-
-    //menu 
     int op;
+    
+    cargaDeArchivo("AyED1.txt", lista); // cargar lista
 
-    op = menuPrincipal ();
-   
-//casos main:
+    do {
+        op = menuPrincipal ();
+        
+        switch (op){
+            case 1:{
+            //-----------LISTA Completa-------------------------------------------------------------------------------
+                showList(lista);
+            } break;
 
+            case 2:{
+            //------Nuevo Elemento------------------------------------------------------------------------------------
+                addNew (lista);
+                saveList ("AyED1.txt", lista);
+            } break;
 
-    switch (op){
-        case 1:{
-    //-----------VER LISTA-------------------------------------------------------
-            showList(lista);
+            case 3:{
+            //------------Modificar elemento--------------------------------------------------------------------------
+                changeOne (lista);
+                saveList ("AyED1.txt", lista);
+            }break;
 
-        } break;
+            case 4:{
+            //-------------Borrar elemento----------------------------------------------------------------------------           
+                deleteOne (lista);
+            } break;
 
-        case 2:{
-    //------Nuevo Elemento-----------------------------------------------------------------------------------------------------------
-            addNew (lista);
-        } break;
+            case 5:{
+            //-------------Carga de Notas-----------------------------------------------------------------------------
+                changeNotes(lista);
+            } break;
 
-        case 3:{
-    //------------Modificar elemento---------------------------------------------------------------------------------------------------
-            changeOne (lista);
+            case 6:{
+            //-------------Lista de Notas------------------------------------------------------------------------------  
+
+            }
         }
-    }
+
+    } while (op < 7);
+
+    cout << "Adios, gracias!!";
 
     return 0;
 }
@@ -97,22 +115,26 @@ void cargaDeArchivo (const string path, tLista& LISTA) {
     return;
 }
 
-int menuPrincipal (){
+int menuPrincipal () {
 
     int op;
     
     do {
         cout << "Que desea hacer?" << endl;
-        cout << "1.- Ver Lista " << endl;
+        cout << "1.- Ver Lista Completa" << endl;
         cout << "2.- Agregar Alumno Nuevo" << endl;
         cout << "3.- Modificar Alumno" << endl;
+        cout << "4.- Eliminar Alumno" << endl;
+        cout << "5.- Cargar Notas" << endl;
+        cout << "6.- Lista de Notas" << endl;
+        cout << "7.- Salir" << endl;
         cin >> op;
-    } while ( op < 1 || op > 3);
+    } while ( op < 1 || op > 7);
 
     return op;
 }
 
-void showList(tLista& LISTA){
+void showList(tLista LISTA) {
 
     cout << "Lista de alumnos (" << LISTA.contador << "):" << endl;
 
@@ -125,20 +147,20 @@ void showList(tLista& LISTA){
     return;
 }
 
-void addNew (tLista& LISTA){
+void addNew (tLista& LISTA) {
 
     cout << "Nuevo alumno.-" << endl;
-    cout << "Nombre/s: " << LISTA.alumno[LISTA.contador].nombre << endl;
-    cout << "Apellido: " << LISTA.alumno[LISTA.contador].apellido << endl;
-    cout << "D.N.I.: " << LISTA.alumno[LISTA.contador].DNI << endl;
-    cout << "Edad: " << LISTA.alumno[LISTA.contador].edad << endl;
-    cout << "Nota: " << LISTA.alumno[LISTA.contador].nota << endl;
+    cout << "Nombre/s: "; cin >> LISTA.alumno[LISTA.contador].nombre;
+    cout << "Apellido: "; cin >> LISTA.alumno[LISTA.contador].apellido;
+    cout << "D.N.I.: "; cin >> LISTA.alumno[LISTA.contador].DNI;
+    cout << "Edad: "; cin >> LISTA.alumno[LISTA.contador].edad;
+    cout << "Nota: "; cin >> LISTA.alumno[LISTA.contador].nota;
     LISTA.contador++;
 
     return;
 }
 
-void changeOne (tLista& LISTA) {
+void changeOne (tLista& LISTA) { //debo crear otra funcion para utilizar adentro
     
     int dni, pos;
 
@@ -178,3 +200,118 @@ void changeOne (tLista& LISTA) {
 
     return;
 }
+
+void saveList (const string path, tLista& LISTA) {
+    ofstream archivoLista;
+    int pos;
+
+    pos = 0;
+
+    archivoLista.open(path);
+
+    if (archivoLista.is_open()) {
+        
+        do {
+            archivoLista << LISTA.alumno[pos].edad << " " <<  LISTA.alumno[pos].DNI << " " << LISTA.alumno[pos].nota << " " << LISTA.alumno[pos].apellido << " " << LISTA.alumno[pos].nombre << endl;
+
+            pos++;  
+
+        } while ( pos < LISTA.contador ); 
+
+    }
+
+    archivoLista.close();
+
+    return;
+}
+
+void deleteOne (tLista& LISTA) { //debo crear otra funcion para utilizar adentro
+    int dni, pos;
+
+    pos = 0;
+
+    cout << "DNI del alumno que desea borrar: ";
+    cin >> dni;
+
+    while ( (LISTA.alumno[pos].DNI != dni) && pos < LISTA.contador ){
+        pos++;
+    }
+        
+    if (LISTA.alumno[pos].DNI == dni){
+
+        for (int i = pos; i< LISTA.contador - 1; i++){
+
+            LISTA.alumno[i].nombre = LISTA.alumno[i+1].nombre;
+    
+            LISTA.alumno[i].apellido = LISTA.alumno[i+1].apellido;
+
+            LISTA.alumno[i].DNI = LISTA.alumno[i+1].DNI;
+
+            LISTA.alumno[i].edad = LISTA.alumno[i+1].edad;
+
+            LISTA.alumno[i].nota = LISTA.alumno[i+1].nota;
+        }
+    }
+    else {
+        cout << "No hubo coincidencia";
+    }
+
+    return;
+}
+
+void changeNotes(tLista& LISTA) { 
+
+    for (int i=0; i < LISTA.contador; i++){
+        cout << LISTA.alumno[i].apellido << ", " << LISTA.alumno[i].nombre << "; " << LISTA.alumno[i].DNI << " - Nota: " << endl;
+        cin >> LISTA.alumno[i].nota;
+    }
+
+    return;
+}
+
+void clasificateNotes (tLista LISTA) {// debo crear otras funciones para que haga una sola cosa
+    int nota, pos;
+    int vectorNotas[50];
+
+    nota = pos = 0;
+
+    cout << "Mejores notas" << endl;
+
+    // Valor nota mas alta
+    for (int i=0; i < LISTA.contador; i++) {
+        if (LISTA.alumno[i].nota > nota) 
+            nota = LISTA.alumno[i].nota;    
+    }
+
+    // vector de posiciones donde tienen la nota mas alta
+    for (int i=0; i < LISTA.contador; i++) {
+        
+        if (LISTA.alumno[i].nota == nota) {
+            vectorNotas[pos] = i;
+            pos++;
+        }
+    }
+
+    // lista de alumnos que tienen la nota mas alta
+    for (int i=0; i < pos; i++) {
+        cout << LISTA.alumno[vectorNotas[i]].apellido << ", " << LISTA.alumno[vectorNotas[i]].nombre << " - Nota: " << LISTA.alumno[vectorNotas[i]].nota << endl; 
+    }
+
+    return;
+}
+
+int calculatePromedio (tLista LISTA) {
+    double suma;
+    double promedio;
+
+    for (int i=0; i < LISTA.contador; i++){
+        suma = suma + LISTA.alumno[i].nota;
+    }
+
+    promedio = suma / LISTA.contador;
+
+    return promedio;
+}
+
+
+
